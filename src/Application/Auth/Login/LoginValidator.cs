58327eb499226f888cdbc06de8073b9cc5f04be8
Yun33bAshraf <yuneeb.a@strahlenstudios.com>
2026-05-01 20:13:33 +0500

    Update LoginValidator.cs

--- src/Application/Auth/Login/LoginValidator.cs
+++ src/Application/Auth/Login/LoginValidator.cs
@@ -1,5 +1,14 @@
 ﻿namespace LMS.Application.Auth.Login;
 
-public class LoginValidator
+public class LoginValidator : AbstractValidator<LoginRequest>
 {
+    public LoginValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required");
+    }
 }
